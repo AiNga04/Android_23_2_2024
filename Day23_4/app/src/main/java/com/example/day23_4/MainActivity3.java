da374eecc6ec061a98033a948c79dbef667d4a2b
AiNga04 <aingaspkt@gmail.com>
2024-02-23 20:51:19 +0700

    22162027

--- Day23_4/app/src/main/java/com/example/day23_4/MainActivity3.java
+++ Day23_4/app/src/main/java/com/example/day23_4/MainActivity3.java
@@ -1,20 +1,68 @@
 package com.example.day23_4;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    Button btn;
-    public void ShowPopupMenu(){
+    Button btn, btn1, btn2, btn3;
+    EditText editText1;
+    Dialog dialog;
+    private void DiaLog1(){
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        editText1 = (EditText) dialog.findViewById(R.id.editTextText);
+        dialog.show();
+    }
+
+    void try2(){
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DiaLog1();
+            }
+        });
+    }
+
+    public void XacNhanXoa(){
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity3.this);
+                alert.setTitle("Thông báo");
+                alert.setMessage("Bạn có muốn đăng xuất không");
+                alert.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alert.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alert.show();
+            }
+        });
+    }
+
+        public void ShowPopupMenu(){
         btn = (Button) findViewById(R.id.btnButton);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +84,25 @@ public class MainActivity3 extends AppCompatActivity {
                 });
             }
         });
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.try1,menu);
+        menu.setHeaderTitle("Context Menu");
+        menu.setHeaderIcon(R.mipmap.ic_launcher);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
 
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuSetting)
+            Toast.makeText(MainActivity3.this,"Setting",Toast.LENGTH_LONG).show();
+        else if(item.getItemId() == R.id.menuShare)
+            Toast.makeText(MainActivity3.this,"Share",Toast.LENGTH_LONG).show();
+        else if(item.getItemId() == R.id.menuLogout)
+            Toast.makeText(MainActivity3.this,"Logout",Toast.LENGTH_LONG).show();
+            return super.onContextItemSelected(item);
     }
 
     @Override
@@ -62,6 +127,12 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        btn1 = (Button) findViewById(R.id.button);
+        registerForContextMenu(btn1);
+        btn2 = (Button) findViewById(R.id.button2);
+        XacNhanXoa();
+        btn3 = (Button) findViewById(R.id.button4);
         ShowPopupMenu();
+        try2();
     }
 }
--- Day23_4/app/src/main/java/com/example/day23_4/MainActivity5.java
+++ Day23_4/app/src/main/java/com/example/day23_4/MainActivity5.java
@@ -5,17 +5,16 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.CheckBox;
-import android.widget.ImageButton;
-import android.widget.RadioGroup;
+import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MainActivity4 extends AppCompatActivity {
-
+public class MainActivity5 extends AppCompatActivity {
     ConstraintLayout bg;
+    Switch sw;
     ArrayList<Integer> arrayList = new ArrayList<>();
     public void rdbackground () {
         arrayList.add(R.drawable.bg2);
@@ -28,10 +27,22 @@ public class MainActivity4 extends AppCompatActivity {
         int vitri = random.nextInt(arrayList.size());
         bg.setBackgroundResource(arrayList.get(vitri));
     }
+
+     void trySwitch()
+     {
+         sw = (Switch) findViewById(R.id.switch2);
+         sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                 rdbackground();
+             }
+         });
+     }
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
-        rdbackground();
+        setContentView(R.layout.activity_main5);
+        trySwitch();
     }
 }
